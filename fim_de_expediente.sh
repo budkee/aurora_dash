f5de651d79331a2586092f45cfe16fcefa63241d
Kaê <kae.budke@gmail.com>
2025-08-18 16:56:25 -0400

    Sprint 2 - Setup ERPNext | 18/08/25

---
+++ fim_de_expediente.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+# 
+# Repositório no GitHub: https://github.com/budkee/aurora_dash
+#
+# Alterar a mensagem conforme o que foi feito no dia 
+echo "\n # -------------- Git add -------------- #"
+git add .
+echo "\n # -------------- Git commit -------------- #"
+git commit -m "Sprint 2 - Setup ERPNext | 18/08/25"
+echo "\n # -------------- Git Push -------------- #"
+git push
+echo "\n # -------------- Git Status -------------- #"
+git status
+
